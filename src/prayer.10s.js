8f742f2d6c7ecc7fd8b77e91db497fea55c946ee
Tahsin <work.tahsin@gmail.com>
2024-10-20 00:02:45 -0400

    fix bug for isha timing

--- src/prayer.10s.js
+++ src/prayer.10s.js
@@ -4,6 +4,14 @@ const moment = require("moment");
 
 const dateToday = moment().format("DD-MM-YYYY");
 const location = "London, ON";
+const prayers = ["Fajr", "Dhuhr", "Asr", "Maghrib", "Isha"];
+
+module.exports.constants = {
+  dateToday,
+  location,
+  prayers,
+  timeNow: moment(),
+};
 
 module.exports.fetchData = async (dateToday, location) => {
   const resp = await new fetch(`https://api.aladhan.com/v1/timingsByAddress/${dateToday}?address=${location}`);
@@ -16,26 +24,8 @@ module.exports.fetchData = async (dateToday, location) => {
 const exec = async () => {
   const { timings, meta, date } = await this.fetchData(dateToday, location);
 
-  const prayers = ["Fajr", "Dhuhr", "Asr", "Maghrib", "Isha"];
-  const timeNow = moment();
-  const nextPrayer = prayers.find((p) => moment(timings[p], "HH:mm").isAfter(timeNow));
-  const currentPrayer = prayers[prayers.indexOf(nextPrayer) ? prayers.indexOf(nextPrayer) - 1 : prayers.length - 1];
-
-  const currentPrayerTime = moment(timings[currentPrayer], "HH:mm");
-  const nextPrayerTime = moment(timings[nextPrayer], "HH:mm");
-  const timeUntilNextPrayer = moment.duration(nextPrayerTime.diff(timeNow));
-
-  const timeInBetween = moment.duration(nextPrayerTime.diff(currentPrayerTime)).asMinutes();
-  const elappsedTime = moment.duration(timeNow.diff(currentPrayerTime)).asMinutes();
-
-  /**
-   * 30 minutes before the next prayer, show the time until the next prayer
-   * 50% of current prayer until next prayer, show current prayer
-   * then show the next prayer and until time
-   */
-  if (timeUntilNextPrayer.asMinutes() < 30) console.log(`🕌 ${nextPrayer} in ${timeUntilNextPrayer.humanize()}`);
-  else if (isMoreThanHalf(timeInBetween, elappsedTime)) console.log(`🕌 ${nextPrayer} in ${timeUntilNextPrayer.humanize()}`);
-  else console.log(`🕌 ${currentPrayer}`);
+  const display = this.formatPrayerDisplay(timings);
+  console.log(`🕌 ${display}`);
 
   console.log("---");
   const arabicDate = `${date.hijri.weekday.en}, ${date.hijri.day} ${date.hijri.month.en} (${date.hijri.month.number}) ${date.hijri.year}`;
@@ -62,5 +52,28 @@ const exec = async () => {
 function isMoreThanHalf(timeInBetween, elappsedTime) {
   return elappsedTime > timeInBetween / 2;
 }
+
+module.exports.formatPrayerDisplay = (timings) => {
+  const nextPrayer = prayers.find((p) => moment(timings[p], "HH:mm").isAfter(this.constants.timeNow)) || prayers[0];
+  const currentPrayer = prayers[prayers.indexOf(nextPrayer) ? prayers.indexOf(nextPrayer) - 1 : prayers.length - 1];
+
+  const currentPrayerTime = moment(timings[currentPrayer], "HH:mm");
+  const nextPrayerTime = moment(timings[nextPrayer], "HH:mm");
+
+  if (nextPrayer === prayers[0]) nextPrayerTime.add(1, "day");
+  const timeUntilNextPrayer = moment.duration(nextPrayerTime.diff(this.constants.timeNow));
+
+  const timeInBetween = moment.duration(nextPrayerTime.diff(currentPrayerTime)).asMinutes();
+  const elappsedTime = moment.duration(this.constants.timeNow.diff(currentPrayerTime)).asMinutes();
+
+  /**
+   * 30 minutes before the next prayer, show the time until the next prayer
+   * 50% of current prayer until next prayer, show current prayer
+   * then show the next prayer and until time
+   */
+  if (timeUntilNextPrayer.asMinutes() < 30) return `${nextPrayer} in ${timeUntilNextPrayer.humanize()}`;
+  else if (isMoreThanHalf(timeInBetween, elappsedTime)) return `${nextPrayer} in ${timeUntilNextPrayer.humanize()}`;
+  else return currentPrayer;
+};
 
 if (require.main === module) exec();
