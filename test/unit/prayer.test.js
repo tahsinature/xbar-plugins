e763f202ca43800fc3097f1af24abd7bb8e99c9f
Tahsin <work.tahsin@gmail.com>
2024-10-20 00:20:33 -0400

    add prayer unit test

---
+++ test/unit/prayer.test.js
@@ -0,0 +1,20 @@
+const { formatPrayerDisplay, constants } = require("../../src/prayer.10s");
+const moment = require("moment");
+
+const timings = {
+  A: "06:26",
+  B: "20:30",
+};
+
+test("it should properly display last & first prayer of the day ", () => {
+  constants.prayers = ["A", "B"];
+  constants.timeNow = moment().set({ hour: 19 });
+
+  expect(formatPrayerDisplay(timings)).toBe("B in an hour");
+
+  constants.timeNow = moment().set({ hour: 23 });
+  expect(formatPrayerDisplay(timings)).toBe("B");
+
+  constants.timeNow = moment().set({ hour: 4 }).add(1, "d");
+  expect(formatPrayerDisplay(timings)).toBe("A in 2 hours");
+});
